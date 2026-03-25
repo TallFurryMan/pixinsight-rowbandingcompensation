4880cdaf160118dbadf4fde181c0b5e1252d8d36
Eric Dejouhanet <eric.dejouhanet@gmail.com>
2026-03-25 15:42:12 +0100

    tune: render residual diagnostics as bars

--- src/scripts/RowBandingCompensation/RowBandingCompensationDiagnostics.js
+++ src/scripts/RowBandingCompensation/RowBandingCompensationDiagnostics.js
@@ -21,18 +21,18 @@ function RowBandingCompensationDiagnosticsExporter( parameters )
       if ( this.parameters.outputRowTrendPlot )
          this.publishProfilePlot( targetId, "rowTrend", profileData.rowTrend );
       if ( this.parameters.outputRowResidualPlot )
-         this.publishProfilePlot( targetId, "rowResidual", profileData.rowResidual );
+         this.publishProfilePlot( targetId, "rowResidual", profileData.rowResidual, null, null, "bar", 0 );
       if ( this.parameters.outputRowInfluencePlot )
          this.publishProfilePlot( targetId, "rowInfluence", rowInfluence, 0, 1 );
       if ( this.parameters.outputRowVisibilityPlot )
-         this.publishProfilePlot( targetId, "rowVisibility", profileData.rowVisibility, 0, 1 );
+         this.publishProfilePlot( targetId, "rowVisibility", profileData.rowVisibility, 0, 1, "bar", 0 );
       if ( this.parameters.outputRowConfidencePlot )
          this.publishProfilePlot( targetId, "rowConfidence", profileData.rowConfidence, 0, 1 );
       if ( this.parameters.outputRowCorrectionPlot )
          this.publishProfilePlot( targetId, "rowCorrection", profileData.rowCorrection );
    };
 
-   this.publishProfilePlot = function( targetId, suffix, values, fixedMinValue, fixedMaxValue )
+   this.publishProfilePlot = function( targetId, suffix, values, fixedMinValue, fixedMaxValue, renderStyle, baselineValue )
    {
       var width = 256 * 3;
       var height = Math.max( 64, values.length );
@@ -65,12 +65,15 @@ function RowBandingCompensationDiagnosticsExporter( parameters )
          valueSpan = maxValue - minValue;
       }
 
+      renderStyle = renderStyle != null ? renderStyle : "point";
+      baselineValue = baselineValue != null ? baselineValue : (minValue < 0 && maxValue > 0 ? 0 : minValue);
+      var baselineX = Math.round( ((baselineValue - minValue) / valueSpan) * (width - 1) );
+      baselineX = rbcClamp( baselineX, 0, width - 1 );
+
       if ( minValue < 0 && maxValue > 0 )
       {
-         var zeroX = Math.round( (-minValue / valueSpan) * (width - 1) );
-         zeroX = rbcClamp( zeroX, 0, width - 1 );
          for ( var zy = 0; zy < height; ++zy )
-            image.setSample( 0.85, zeroX, zy );
+            image.setSample( 0.85, baselineX, zy );
       }
 
       for ( var i = 0; i < values.length; ++i )
@@ -83,11 +86,21 @@ function RowBandingCompensationDiagnosticsExporter( parameters )
          x = rbcClamp( x, 0, width - 1 );
          y = rbcClamp( y, 0, height - 1 );
 
-         image.setSample( 0.05, x, y );
-         if ( x > 0 )
-            image.setSample( 0.25, x - 1, y );
-         if ( x < width - 1 )
-            image.setSample( 0.25, x + 1, y );
+         if ( renderStyle == "bar" )
+         {
+            var x0 = Math.min( baselineX, x );
+            var x1 = Math.max( baselineX, x );
+            for ( var xx = x0; xx <= x1; ++xx )
+               image.setSample( xx == x ? 0.05 : 0.25, xx, y );
+         }
+         else
+         {
+            image.setSample( 0.05, x, y );
+            if ( x > 0 )
+               image.setSample( 0.25, x - 1, y );
+            if ( x < width - 1 )
+               image.setSample( 0.25, x + 1, y );
+         }
 
          progress( i + 1 );
       }
